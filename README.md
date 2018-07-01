a5dd00dbe8a3f6234efa5c57a51be77c2d115a42
Steph Gagnon <sgagnon@stanford.edu>
2018-07-01 11:37:36 -0400

    Init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+felix
+===========
+
+Wagner Lab helper functions for presenting stimuli and collecting behavioral data.
+
+
+### Acknowledgements
+
+Thanks to Amitai Shenhav for scripts/MATLAB code snippets that have been used/modified in this repository.
---
+++ scripts/getColors.m
@@ -0,0 +1,12 @@
+function [c] = getColors()
+
+c.white=[255 255 255];
+c.black=[0 0 0];
+c.grey = [164 164 164];
+c.darkgray = [96 96 96];
+c.green = [34 139 34];
+c.red = [255 0 0];
+c.teal = [2 197 171]; % 65percent luminance
+c.orange = [255 126 56]; % 65percent luminance
+
+% Luminance http://www.workwithcolor.com/hsl-color-picker-01.htm
